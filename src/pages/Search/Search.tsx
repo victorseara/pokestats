24d0384d682f772ba2511327abe7ccbf72026337
victorseara <victorseara.me@gmail.com>
2021-10-12 16:04:21 -0300

    lazy loading error fallback and add error boundarie

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,13 +1,24 @@
+import Loader from "components/Loader/Loader";
 import PageLayout from "components/PageLayout/PageLayout";
+import React, { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { RouteComponentProps } from "react-router";
 import PokemonSearch from "./components/PokemonSearch/PokemonSearch";
+
+const PokemonFallback = React.lazy(
+  () => import("components/PokemonFallback/PokemonFallback")
+);
 
 const SearchResult = ({ location }: RouteComponentProps) => {
   const query = location.pathname.replace("/", "");
 
   return (
     <PageLayout>
-      <PokemonSearch pokemonName={query} />
+      <Suspense fallback={Loader}>
+        <ErrorBoundary FallbackComponent={PokemonFallback} resetKeys={[query]}>
+          <PokemonSearch pokemonName={query} />
+        </ErrorBoundary>
+      </Suspense>
     </PageLayout>
   );
 };
